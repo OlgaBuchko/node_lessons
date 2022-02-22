54b9c74d306d5066e68162b1c0ce65a12495ed69
OlgaBuchko <lyolyabo92@gmail.com>
2022-02-22 19:31:29 +0200

    g

--- app.js
+++ app.js
@@ -2,45 +2,45 @@
 // просто зробити темплейт з цим усім і вводити свої дані які будуть пушитися в масив і редірект робити на сторінку з усіма
 // юзерами /users і перевірка чи такий імейл не існує, якщо існує то редірект на еррор пейдж
 
-const path = require ('path')
-const express = require ('express');
-const {engine} = require ('express-handlebars');
+const path = require('path')
+const express = require('express');
+const {engine} = require('express-handlebars');
 const {json} = require("express");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname,'static')));
-app.set('view engine','.hbs');
-app.engine('.hbs',engine({defaultLayout:false}));
-app.set('views',path.join(__dirname, 'static'))
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'static'))
 
 const users = [];
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('server has started on port 5000')
 });
 
-app.get('/login',(req, res) =>{
-res.render('login')
-} );
+app.get('/login', (req, res) => {
+    res.render('login')
+});
 
-app.get('/users',({query}, res) => {
-    const {city, age}=query;
+app.get('/users', ({query}, res) => {
+    const {city, age} = query;
 
-    if(city||age){
-        usersArr=[...users]
-        if (city){
-         res.json(usersArr.filter(user=>user.city === query.city));
+    if (city || age) {
+        usersArr = [...users]
+        if (city) {
+            res.json(usersArr.filter(user => user.city === query.city));
 
         }
-     if (age){
-         res.json(usersArr.filter(user=>user.age === query.age));
+        if (age) {
+            res.json(usersArr.filter(user => user.age === query.age));
 
-     }
+        }
     }
-    res.render('users',{users})
+    res.render('users', {users})
 })
 
 app.post('/login', ({body}, res) => {
@@ -51,10 +51,9 @@ app.post('/login', ({body}, res) => {
         return;
     }
 
-    users.push({ ...body,id:users.length+1});
+    users.push({...body, id: users.length + 1});
     res.redirect('/users');
 });
-
 
 
 // 2. /users просто сторінка з усіма юзерами, але можна по квері параметрам їх фільтрувати по age і city
@@ -62,7 +61,7 @@ app.post('/login', ({body}, res) => {
 
 // 3. /user/:id сторінка з інфою про одного юзера
 
-app.get('/users/:Id', ({ params }, res) => {
+app.get('/users/:Id', ({params}, res) => {
     const user = users.find(user => user.id === +params.Id);
     if (!user) {
         error = `User with ID: ${params.userId} exist!`;
@@ -70,11 +69,11 @@ app.get('/users/:Id', ({ params }, res) => {
         return;
     }
 
-    res.render('user', { user });
+    res.render('user', {user});
 });
 
 app.get('/error', (req, res) => {
-    res.render('error', { error });
+    res.render('error', {error});
 });
 // 4. зробити якщо не відпрацюють ендпоінти то на сторінку notFound редірект
 app.use((req, res) => {
